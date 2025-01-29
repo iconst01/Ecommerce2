2b9bc011888f1de4b4526c908fe93e835e2c2481
Itaty <itatyconstante@gmail.com>
2025-01-29 17:00:34 -0500

    final product

--- ecommerce/server/server.js
+++ ecommerce/server/server.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
-require('dotenv').config();
 
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/images', express.static('/public/images'));
+app.use(express.static(path.join(__dirname, "../dist")));
+
 
 // Set up MySQL connection
 const db = mysql.createConnection({
@@ -54,6 +62,12 @@ app.get('/api/products', (req, res) => {
     res.json(results);
   });
 });
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"))
+})
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
