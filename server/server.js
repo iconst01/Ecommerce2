180c2352a02f797a090e1069fce737b2f2966742
Itaty <itatyconstante@gmail.com>
2025-01-29 17:16:38 -0500

    port fixed

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"))
 })
 
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
